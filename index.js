b4548281050512a9884a29891d3baeb33b7d672c
tarpaha <ppopchikovsky@gmail.com>
2018-08-17 11:19:49 +0300

    test.html was renamed to index.html, abd returned from server root

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-const browserSync = require('browser-sync');
-
 const express = require('express');
 const app = express();
 const port = 3000;
 
 const isProduction = 'production' === process.env.NODE_ENV;
 
+app.use(express.static('public'));
 app.get('/', function (req, res) {
-  res.send('<body>Hello World!</body>');
+  res.sendFile('index.html');
 });
 
-app.use(express.static('public'));
+function staticResourcesSync() {
+    const browserSync = require('browser-sync');
+    browserSync({
+        files: ['public/**/*'],
+        online: false,
+        open: false,
+        port: port,
+        proxy: 'localhost:' + port,
+        ui: false
+    });
+}
 
 app.listen(port, function () {
     console.log(`Example app listening on port ${port}!`);
     if(!isProduction) {
-        browserSync({
-            files: ['**/*'],
-            online: false,
-            open: false,
-            port: port,
-            proxy: 'localhost:' + port,
-            ui: false
-        });
+        staticResourcesSync();
     }
 });
